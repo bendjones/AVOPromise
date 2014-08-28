f593a8b69988ef790712385f8572c272106da690
Ben D. Jones <ben.d.jones@me.com>
2014-08-28 02:58:39 -0700

    Initial Commit

---
+++ AVOPromise/AVOPromise.h
@@ -0,0 +1,19 @@
+//
+//  AVOPromise.h
+//  AVOPromise
+//
+//  Created by Ben D. Jones on 8/28/14.
+//  Copyright (c) 2014 Ben D. Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVOPromise.
+FOUNDATION_EXPORT double AVOPromiseVersionNumber;
+
+//! Project version string for AVOPromise.
+FOUNDATION_EXPORT const unsigned char AVOPromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVOPromise/PublicHeader.h>
+
+
